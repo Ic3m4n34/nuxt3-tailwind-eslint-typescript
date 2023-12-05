c94e41f07eb97d8749cdc567a966a51696cfafbf
Nico Meyer <nicomeyer@hey.com>
2023-12-05 19:34:08 +0100

    chore: update dependencies

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,19 @@
 <template>
-  <div class="layout--default p-8">
-    <!-- <ul class="flex flex-row space-x-4">
-      <nuxt-link
-        v-for="link in links"
-        :key="link.path"
-        :to="link.path"
-        class="inline-block"
-      >
-        {{ link.name }}
-      </nuxt-link>
-    </ul> -->
+  <div class="layout--default">
     <slot />
   </div>
 </template>
 
 <script setup lang="ts">
-/* const links = ref([
-  {
-    name: 'Home',
-    path: '/',
-  },
-  {
-    name: 'Other Page',
-    path: '/other-page',
-  },
-]); */
+useHead({
+  title: 'My Page',
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'a description',
+    },
+  ],
+});
 
 </script>
